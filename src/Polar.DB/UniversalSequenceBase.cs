d32c12a58f670d9f47beaf4e367c914ef3e2f1b9
Sergey303 <s-agittariu-s@mail.ru>
2026-04-24 22:51:59 +0700

    отдельный проект под Nuget версию Polar.DB

--- src/Polar.DB/UniversalSequenceBase.cs
+++ src/Polar.DB/UniversalSequenceBase.cs
@@ -724,7 +724,7 @@ public class UniversalSequenceBase
     ///     При ошибке длина файла, логический конец и количество элементов
     ///     откатываются к исходному состоянию до начала bulk-записи.
     /// </remarks>
-    public void AppendElements(IEnumerable<object> flow, Action<(object element, long off)>? apply = null)
+    public void AppendElements(IEnumerable<object> flow, Action<object, long>? onAppended = null)
     {
         _ = flow ?? throw new ArgumentNullException(nameof(flow));
 
@@ -737,31 +737,30 @@ public class UniversalSequenceBase
 
         try
         {
-            if( fs.Position != AppendOffset)
+            if (fs.Position != AppendOffset)
                 fs.Position = AppendOffset;
 
             foreach (var element in flow)
             {
                 _ = element ?? throw new ArgumentNullException(nameof(flow), "Sequence flow contains a null element.");
+
+                long off = fs.Position;
                 ByteFlow.Serialize(bw, element, tp_elem);
                 nelements += 1;
-                if (apply == null)
-                    continue;
-                apply(new ValueTuple<object, long>(element, savedPosition));
+                onAppended?.Invoke(element, off);
             }
 
-            AppendOffset = fs.Length;
+            AppendOffset = fs.Position;
             EnsureAppendOffsetInvariant();
         }
         catch
         {
             if (fs.Length != originalLength)
-            {
                 fs.SetLength(originalLength);
-            }
 
             AppendOffset = originalAppendOffset;
             nelements = originalCount;
+            fs.Position = AppendOffset;
             throw;
         }
         finally
@@ -770,6 +769,7 @@ public class UniversalSequenceBase
         }
     }
 
+<<<<<<< HEAD
     /// <summary>
     ///     Добавляет поток элементов в логический конец последовательности
     ///     с одним позиционированием в хвост и без лишнего восстановления позиции
