f1ee591e452e286c9b6f5d454414b32744f95455
Sergey303 <s-agittariu-s@mail.ru>
2026-05-08 20:35:16 +0700

    добавление  эксперимента like поиск строк: удаление старых файлов

--- benchmarks/src/Polar.DB.Bench.Core/StringLikeLookup/ArtifactInfo.cs
+++
@@ -1,3 +0,0 @@
-namespace Polar.DB.Bench.Core.StringLikeLookup;
-
-public sealed record ArtifactInfo(string Role, string Path, long Bytes);
--- benchmarks/src/Polar.DB.Bench.Engine.PolarDb/StringLikeLookup/PolarDbLikeLookupEngineOptions.cs
+++
@@ -1,5 +0,0 @@
-namespace Polar.DB.Bench.Engine.PolarDb.StringLikeLookup;
-
-public sealed record PolarDbLikeLookupEngineOptions(
-    string WorkDirectory,
-    string EngineKey = "polar-db-current");
