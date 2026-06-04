15b4fc8412a1d0b1e2d8a1d79c9e65017393390b
Leshtaev Sergey <lumenveritatis303@gmail.com>
2026-06-04 12:30:42 +0700

    CompactAsync не блокирует поиски и изменения в индексе до последнего этапа замены файлов, добавлены lock

--- src/Polar.DB/ExternalKey/ExternalKeyIndexEntry.cs
+++ src/Polar.DB/ExternalKey/ExternalKeyIndexEntry.cs
@@ -3,14 +3,16 @@ namespace Polar.DB.ExternalKey;
 internal readonly struct ExternalKeyIndexEntry<T>
     where T : IComparable<T>
 {
-    internal ExternalKeyIndexEntry(IComparable primary, T key, long offset)
+    internal ExternalKeyIndexEntry(IComparable primary, T key, long offset, long revision)
     {
         Primary = primary;
         Key = key;
         Offset = offset;
+        Revision = revision;
     }
 
     internal IComparable Primary { get; }
     internal T Key { get; }
     internal long Offset { get; }
+    internal long Revision { get; }
 }
