0b513a071cb611b64fcbf2a1b744be6ac86481d0
Sergey303 <s-agittariu-s@mail.ru>
2026-07-06 06:47:26 +0300

    Report parallel and sequential SetPrimaryKey offset-set A/B

--- benchmarks/src/BenchSupport/LifecycleBench.cs
+++ benchmarks/src/BenchSupport/LifecycleBench.cs
@@ -56,8 +56,10 @@ internal static class LifecycleBench
                 options, data, Path.Combine(caseDir, "polar-fixed64-typed-metadata-probe")));
             engines.Add(PolarLifecycleEngine.RunFixedInt64TypedBuildProbePrimaryIntOnly(
                 options, data, Path.Combine(caseDir, "polar-fixed64-typed-build-probe")));
-            engines.Add(SetPrimaryKeyLifecycleEngine.Run(
+            engines.Add(SetPrimaryKeyLifecycleEngine.RunParallel(
                 options, data, Path.Combine(caseDir, "polar-set-primary-key-fixed64")));
+            engines.Add(SetPrimaryKeyLifecycleEngine.RunSequential(
+                options, data, Path.Combine(caseDir, "polar-set-primary-key-fixed64-sequential-offset-set")));
         }
 
         return engines;
