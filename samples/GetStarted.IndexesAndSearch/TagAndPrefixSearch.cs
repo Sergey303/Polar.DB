a706e2c196c12dbe28357b759602c9d3cf35eb91
Sergey303 <s-agittariu-s@mail.ru>
2026-07-07 14:54:44 +0300

    Move tag and prefix sample to SetPrimaryKey

--- samples/GetStarted.IndexesAndSearch/TagAndPrefixSearch.cs
+++ samples/GetStarted.IndexesAndSearch/TagAndPrefixSearch.cs
@@ -55,14 +55,14 @@ internal static class TagAndPrefixSearch
     private static USequence CreateSequence(string dbPath)
     {
         var nextStreamIndex = 0;
-        return new USequence(
+        var sequence = new USequence(
             ExtendPeopleScheme.RecordType,
             stateFileName: null,
             streamGen: () => OpenStream(dbPath, $"primary-{nextStreamIndex++:00}.bin"),
             isEmpty: _ => false,
-            keyFunc: record => ExtendPeopleScheme.Id(record),
-            hashOfKey: key => Convert.ToInt32(key),
             optimise: true);
+        sequence.SetPrimaryKey<int>(record => ExtendPeopleScheme.Id(record));
+        return sequence;
     }
 
     private static Func<Stream> CreateStreamFactory(string dbPath, string prefix)
