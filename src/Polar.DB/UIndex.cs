65a92a9c0ce069d9fd5e8ba684aca25e7ee25f12
Sergey303 <s-agittariu-s@mail.ru>
2026-08-08 16:31:24 +0700

    Defer dynamic UIndex sorting to query time

--- src/Polar.DB/UIndex.cs
+++ src/Polar.DB/UIndex.cs
@@ -12,179 +12,167 @@ namespace Polar.Universal
     public class UIndex : IUIndex
     {
         private readonly USequence sequence;
+        private readonly Func<object, bool> applicable;
+        private readonly Func<object, int>? hashFunc;
+        private readonly Comparer<object> comp;
 
-        // Параметры конструктора
-        private Func<object, bool> applicable;
-        private Func<object, int> hashFunc;
-        private Comparer<object> comp;
+        private readonly UniversalSequenceBase? hkeys;
+        private readonly UniversalSequenceBase offsets;
 
-        // Статическая часть индекса
-        private UniversalSequenceBase hkeys;
-        private UniversalSequenceBase offsets;
+        // Appends stay O(1). The historical sorted enumeration order is restored lazily
+        // before the next dynamic query instead of re-sorting after every append.
+        private readonly List<HKeyObjOff> dynset = new();
+        private readonly Comparer<HKeyObjOff> complexComp;
+        private bool dynsetSorted = true;
 
-        // Динамическая часть индекса: множество троек и компаратор
-        private HKeyObjOff[] dynset;
-        private Comparer<HKeyObjOff> complex_comp;
+        private int[]? hkeysArr;
+        private Comparer<long>? compSpecLong;
+        private bool disposed;
 
-        public UIndex(Func<Stream> streamGen, USequence sequence,
-            Func<object, bool> applicable, Func<object, int> hashFunc, Comparer<object> comp)
+        public UIndex(
+            Func<Stream> streamGen,
+            USequence sequence,
+            Func<object, bool> applicable,
+            Func<object, int>? hashFunc,
+            Comparer<object> comp)
         {
-            this.sequence = sequence;
-            this.applicable = applicable;
+            _ = streamGen ?? throw new ArgumentNullException(nameof(streamGen));
+            this.sequence = sequence ?? throw new ArgumentNullException(nameof(sequence));
+            this.applicable = applicable ?? throw new ArgumentNullException(nameof(applicable));
             this.hashFunc = hashFunc;
-            this.comp = comp;
+            this.comp = comp ?? throw new ArgumentNullException(nameof(comp));
 
-            if (hashFunc != null) hkeys = new UniversalSequenceBase(new PType(PTypeEnumeration.integer), streamGen());
+            if (hashFunc != null)
+                hkeys = new UniversalSequenceBase(new PType(PTypeEnumeration.integer), streamGen());
             offsets = new UniversalSequenceBase(new PType(PTypeEnumeration.longinteger), streamGen());
 
-            complex_comp = Comparer<HKeyObjOff>.Create(new Comparison<HKeyObjOff>((HKeyObjOff h1, HKeyObjOff h2) =>
+            complexComp = Comparer<HKeyObjOff>.Create((left, right) =>
             {
-                int cmp;
-                if (hashFunc != null)
+                if (this.hashFunc != null)
                 {
-                    cmp = h1.hkey.CompareTo(h2.hkey);
-                    if (cmp != 0) return cmp;
+                    int hashComparison = left.hkey.CompareTo(right.hkey);
+                    if (hashComparison != 0) return hashComparison;
                 }
 
-                return comp.Compare(h1.obj, h2.obj);
-            }));
-            dynset = new HKeyObjOff[0];
+                return this.comp.Compare(left.obj, right.obj);
+            });
         }
-
-        private int[] hkeys_arr = null;
-        private Comparer<long> comp_spec_long;
-        private bool disposed;
 
         public void Clear()
         {
-            if (hashFunc != null) hkeys.Clear();
-            hkeys_arr = null;
+            hkeys?.Clear();
+            hkeysArr = null;
             offsets.Clear();
-            dynset = new HKeyObjOff[0];
+            ClearDynamic();
         }
 
         public void Flush()
         {
-            if (hashFunc != null) hkeys.Flush();
+            hkeys?.Flush();
             offsets.Flush();
         }
 
-        public void Close()
-        {
-            Dispose();
-        }
+        public void Close() => Dispose();
 
         public void Refresh()
         {
             if (hashFunc != null)
             {
-                hkeys.Refresh();
-                hkeys_arr = hkeys.ElementValues().Cast<int>().ToArray();
+                hkeys!.Refresh();
+                hkeysArr = hkeys.ElementValues().Cast<int>().ToArray();
             }
 
             offsets.Refresh();
-            dynset = new HKeyObjOff[0];
+            ClearDynamic();
         }
 
         public void Build()
         {
             if (hashFunc == null) BuildOffsets();
             else BuildHkeyOffsets();
+
+            // Build scans the complete current sequence, so all dynamic rows are now
+            // represented by the static snapshot and must not be emitted twice.
+            ClearDynamic();
         }
 
         private void BuildOffsets()
         {
-            comp_spec_long = Comparer<long>.Create(new Comparison<long>((off1, off2) =>
+            compSpecLong = Comparer<long>.Create((off1, off2) =>
             {
                 object v1 = sequence.GetByOffset(off1);
                 object v2 = sequence.GetByOffset(off2);
                 return comp.Compare(v1, v2);
-            }));
-            // сканируем опорную последовательность, формируем массивы
-            List<long> offsets_list = new ();
+            });
+
+            var offsetsList = new List<long>();
             sequence.Scan((off, obj) =>
             {
-                if (applicable(obj)) offsets_list.Add(off);
+                if (applicable(obj)) offsetsList.Add(off);
                 return true;
             });
 
-            long[] offsets_arr = offsets_list.ToArray();
-            offsets_list = null;
-            GC.Collect();
-
-            Array.Sort(offsets_arr, comp_spec_long);
-
-            offsets.Clear();
-            foreach (var off in offsets_arr) offsets.AppendElement(off);
-            offsets.Flush();
-            offsets_arr = null;
-            GC.Collect();
+            long[] offsetsArray = offsetsList.ToArray();
+            Array.Sort(offsetsArray, compSpecLong);
+            offsets.ReplaceWithFixedInt64Array(offsetsArray);
         }
 
         private void BuildHkeyOffsets()
         {
-            // сканируем опорную последовательность, формируем массивы
-            List<int> hkeys_list = new ();
-            List<long> offsets_list = new ();
+            var hkeysList = new List<int>();
+            var offsetsList = new List<long>();
             sequence.Scan((off, obj) =>
             {
                 if (applicable(obj))
                 {
-                    offsets_list.Add(off);
-                    hkeys_list.Add(hashFunc(obj));
+                    offsetsList.Add(off);
+                    hkeysList.Add(hashFunc!(obj));
                 }
 
                 return true;
             });
 
-            hkeys_arr = hkeys_list.ToArray();
-            hkeys_list = null;
-            long[] offsets_arr = offsets_list.ToArray();
-            offsets_list = null;
-            GC.Collect();
+            hkeysArr = hkeysList.ToArray();
+            long[] offsetsArray = offsetsList.ToArray();
+            Array.Sort(hkeysArr, offsetsArray);
 
-            Array.Sort(hkeys_arr, offsets_arr);
-
-            hkeys.Clear();
-            foreach (var hkey in hkeys_arr) hkeys.AppendElement(hkey);
-            hkeys.Flush();
-
-            offsets.Clear();
-            foreach (var off in offsets_arr) offsets.AppendElement(off);
-            offsets.Flush();
-            offsets_arr = null;
-            GC.Collect();
+            hkeys!.ReplaceWithFixedInt32Array(hkeysArr);
+            offsets.ReplaceWithFixedInt64Array(offsetsArray);
         }
 
         internal IEnumerable<ObjOff> GetAllBySample(object sample)
         {
-            if (dynset.Length > 0)
-            {
-                HKeyObjOff complex_sample = new () { obj = sample };
-                if (hashFunc != null) complex_sample.hkey = hashFunc(sample);
+            if (sample == null) throw new ArgumentNullException(nameof(sample));
 
-                var query = dynset.Where(hoo => complex_comp.Compare(hoo, complex_sample) == 0)
-                    .Select(hoo => new ObjOff(hoo.obj, hoo.off));
-                foreach (var oo in query)
-                {
-                    yield return oo;
-                }
+            if (dynset.Count > 0)
+            {
+                EnsureDynamicSorted();
+                var dynamicSample = new HKeyObjOff { obj = sample };
+                if (hashFunc != null) dynamicSample.hkey = hashFunc(sample);
+
+                int start = LowerBoundDynamic(dynamicSample);
+                for (int i = start; i < dynset.Count && complexComp.Compare(dynset[i], dynamicSample) == 0; i++)
+                    yield return new ObjOff(dynset[i].obj, dynset[i].off);
             }
 
             if (hashFunc != null)
             {
-                int hsample = hashFunc(sample);
-                long firstByHash = GetFirstNom(hsample);
+                int hashSample = hashFunc(sample);
+                long firstByHash = GetFirstNom(hashSample);
                 if (firstByHash == -1) yield break;
 
-                for (long ii = firstByHash; ii < hkeys.Count(); ii++)
+                long count = hkeysArr?.LongLength ?? hkeys!.Count();
+                for (long ii = firstByHash; ii < count; ii++)
                 {
-                    int hkey = (int)hkeys.GetByIndex(ii);
-                    if (hkey != hsample) break;
+                    int hashKey = hkeysArr != null
+                        ? hkeysArr[(int)ii]
+                        : (int)hkeys!.GetByIndex(ii);
+                    if (hashKey != hashSample) break;
 
                     long off = (long)offsets.GetByIndex(ii);
                     object value = sequence.GetByOffset(off);
-                    if (comp.Compare(value, sample) == 0) yield return new ObjOff(value, off);
+                    if (comp.Compare(value, sample) == 0)
+                        yield return new ObjOff(value, off);
                 }
 
                 yield break;
@@ -196,30 +184,38 @@ namespace Polar.Universal
             {
                 long off = (long)offsets.GetByIndex(ii);
                 object value = sequence.GetByOffset(off);
-                if (comp.Compare(value, sample) == 0) yield return new ObjOff(value, off);
-                else break;
+                if (comp.Compare(value, sample) == 0)
+                    yield return new ObjOff(value, off);
+                else
+                    break;
             }
         }
 
-        internal IEnumerable<ObjOff> GetAllByLike(object sample, Comparer<object> comp_like)
+        internal IEnumerable<ObjOff> GetAllByLike(object sample, Comparer<object> compLike)
         {
-            if (dynset.Length > 0)
+            if (sample == null) throw new ArgumentNullException(nameof(sample));
+            if (compLike == null) throw new ArgumentNullException(nameof(compLike));
+
+            if (dynset.Count > 0)
             {
-                var query = dynset.Select(hoo => new ObjOff(hoo.obj, hoo.off));
-                foreach (var oo in query)
+                EnsureDynamicSorted();
+                foreach (var item in dynset)
                 {
-                    if (comp_like.Compare(oo.obj, sample) == 0) yield return oo;
+                    if (compLike.Compare(item.obj, sample) == 0)
+                        yield return new ObjOff(item.obj, item.off);
                 }
             }
 
-            long first = GetFirstNomOffsets(sample, comp_like);
+            long first = GetFirstNomOffsets(sample, compLike);
             if (first == -1) yield break;
             for (long ii = first; ii < offsets.Count(); ii++)
             {
                 long off = (long)offsets.GetByIndex(ii);
                 object value = sequence.GetByOffset(off);
-                if (comp_like.Compare(value, sample) == 0) yield return new ObjOff(value, off);
-                else break;
+                if (compLike.Compare(value, sample) == 0)
+                    yield return new ObjOff(value, off);
+                else
+                    break;
             }
         }
 
@@ -227,14 +223,39 @@ namespace Polar.Universal
         {
             if (!applicable(element)) return;
 
-            HKeyObjOff item = new HKeyObjOff() { obj = element, off = offset };
+            var item = new HKeyObjOff { obj = element, off = offset };
             if (hashFunc != null) item.hkey = hashFunc(element);
 
-            HKeyObjOff[] updated = new HKeyObjOff[dynset.Length + 1];
-            Array.Copy(dynset, updated, dynset.Length);
-            updated[updated.Length - 1] = item;
-            Array.Sort(updated, complex_comp);
-            dynset = updated;
+            dynset.Add(item);
+            dynsetSorted = dynset.Count <= 1;
+        }
+
+        private void EnsureDynamicSorted()
+        {
+            if (dynsetSorted) return;
+            dynset.Sort(complexComp);
+            dynsetSorted = true;
+        }
+
+        private int LowerBoundDynamic(HKeyObjOff sample)
+        {
+            int left = 0;
+            int right = dynset.Count;
+            while (left < right)
+            {
+                int middle = left + (right - left) / 2;
+                if (complexComp.Compare(dynset[middle], sample) < 0)
+                    left = middle + 1;
+                else
+                    right = middle;
+            }
+            return left;
+        }
+
+        private void ClearDynamic()
+        {
+            dynset.Clear();
+            dynsetSorted = true;
         }
 
         private long GetFirstNomOffsets(object sample, Comparer<object> comparer)
@@ -246,8 +267,8 @@ namespace Polar.Universal
             while (left < right)
             {
                 long middle = left + (right - left) / 2;
-                object middle_value = sequence.GetByOffset((long)offsets.GetByIndex(middle));
-                int cmp = comparer.Compare(middle_value, sample);
+                object middleValue = sequence.GetByOffset((long)offsets.GetByIndex(middle));
+                int cmp = comparer.Compare(middleValue, sample);
                 if (cmp < 0) left = middle + 1;
                 else right = middle;
             }
@@ -257,28 +278,38 @@ namespace Polar.Universal
             return comparer.Compare(value, sample) == 0 ? left : -1;
         }
 
-        /// <summary>
-        /// Определение номера первого индекса последовательности hkeys, с которого значения РАВНЫ hkey (хешу от ключа).
-        /// Если нет таких, то -1L.
-        /// </summary>
         private long GetFirstNom(int hkey)
         {
-            long count = hkeys.Count();
-            long left = 0;
-            long right = count;
-
-            while (left < right)
+            if (hkeysArr != null)
             {
-                long middle = left + (right - left) / 2;
-                int middleValue = (int)hkeys.GetByIndex(middle);
-                if (middleValue < hkey) left = middle + 1;
-                else right = middle;
+                int left = 0;
+                int right = hkeysArr.Length;
+                while (left < right)
+                {
+                    int middle = left + (right - left) / 2;
+                    if (hkeysArr[middle] < hkey) left = middle + 1;
+                    else right = middle;
+                }
+
+                if (left >= hkeysArr.Length || hkeysArr[left] != hkey) return -1L;
+                return left;
             }
 
-            if (left >= count) return -1;
-            return (int)hkeys.GetByIndex(left) == hkey ? left : -1;
-        }
-        
+            long count = hkeys!.Count();
+            long diskLeft = 0;
+            long diskRight = count;
+            while (diskLeft < diskRight)
+            {
+                long middle = diskLeft + (diskRight - diskLeft) / 2;
+                int middleValue = (int)hkeys.GetByIndex(middle);
+                if (middleValue < hkey) diskLeft = middle + 1;
+                else diskRight = middle;
+            }
+
+            if (diskLeft >= count) return -1L;
+            return (int)hkeys.GetByIndex(diskLeft) == hkey ? diskLeft : -1L;
+        }
+
         public void Dispose()
         {
             Dispose(true);
@@ -288,10 +319,9 @@ namespace Polar.Universal
         private void Dispose(bool disposing)
         {
             if (!disposing || disposed) return;
-            if (hashFunc != null) hkeys.Dispose();
+            hkeys?.Dispose();
             offsets.Dispose();
             disposed = true;
         }
-
     }
 }
